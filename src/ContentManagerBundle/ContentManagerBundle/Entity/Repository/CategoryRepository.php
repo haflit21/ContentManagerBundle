4825c75c851dbc703eba7cb720f6d44b1469cb59
Abdoula <haflit21@hotmail.fr>
2013-03-02 17:33:49 +0100

    Category Translation

---
+++ src/ContentManagerBundle/ContentManagerBundle/Entity/Repository/CategoryRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ContentManagerBundle\ContentManagerBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * CategoryRepository
+ */
+class CategoryRepository extends EntityRepository
+{
+	function getCategoriesByLangId($lang){
+	    return $this->_em
+	    			->createQueryBuilder()
+	    			->select('c')
+	       			->from('ContentManagerBundle:CMCategory', 'c')
+	       			->leftjoin('c.language', 'l')
+	       			->where('l.id = :id')
+	         		->setParameter('id', $lang)
+	    			->getQuery()
+	               	->getResult();
+	}
+
+	function getCategoriesByLangIso($lang){
+	    return $this->_em
+	    			->createQueryBuilder()
+	    			->select('c')
+	       			->from('ContentManagerBundle:CMCategory', 'c')
+	       			->leftjoin('c.language', 'l')
+	       			->where('l.iso = :iso')
+	         		->setParameter('iso', $lang)
+	    			->getQuery()
+	               	->getResult();
+	}
+}
--- src/ContentManagerBundle/ContentManagerBundle/Type/ContentType.php
+++ src/ContentManagerBundle/ContentManagerBundle/Type/ContentType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+use ContentManagerBundle\ContentManagerBundle\Entity\Repository\CategoryRepository;
+
 class ContentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -41,6 +43,10 @@ class ContentType extends AbstractType
             ))*/
             ->add('categories', 'entity', array(
                 'class'=>'ContentManagerBundle:CMCategory',
+                'query_builder' => function(CategoryRepository $er) {
+    var_dump($options);die();
+                    return $er->getCategoriesByLangIso($options['lang']);
+                },
                 'label'=>'Categories',
                 'property'=>'title',
                 'expanded'=>false,
@@ -56,7 +62,8 @@ class ContentType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'ContentManagerBundle\ContentManagerBundle\Entity\CMContent'
+            'data_class' => 'ContentManagerBundle\ContentManagerBundle\Entity\CMContent',
+            'lang' => 'fr-FR'
         ));
     }
 
